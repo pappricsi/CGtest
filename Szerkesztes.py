4b695e8a2322f70ad6416cda779d25388cc91e98
Papp Richárd <papprichard97@gmail.com>
2020-09-23 12:25:31 +0200

    nyersanyag szerkesztés

--- Szerkesztes.py
+++ Szerkesztes.py
@@ -8,8 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
 
-
-class Test2(unittest.TestCase):
+class Test(unittest.TestCase):
     options = webdriver.ChromeOptions()
     options.add_argument("--auto-open-devtools-for-tabs")
     driver = webdriver.Chrome()
@@ -31,69 +30,45 @@ class Test2(unittest.TestCase):
         element.send_keys(login)
         element = self.driver.find_element_by_xpath('//*[@id="login"]/button').click()
 
-        # Raktarkészlet menüpont megnyitasa
+        #Raktarkészlet menüpont megnyitasa
         self.driver.find_element_by_xpath("/html/body/section/div/a[3]/span").click()
-        # Uj nyersanyag gomb
+        #Uj nyersanyag gomb
         self.driver.find_element_by_xpath("//*[@id='newComponent']").click()
-        # Termék létrehozása nyitókészlettel
-        # nyersanyag adatok kitoltese + váltás iframe-re a böngészőben
+        # Termék létrehozása nyitókészlet megadása nélkül
+        #nyersanyag adatok kitoltese + váltás iframe-re a böngészőben
         self.driver.implicitly_wait(3)
         self.driver.switch_to.frame(self.driver.find_element_by_tag_name("iframe"))
-        # név
+        #név
         self.driver.find_element_by_xpath("//*[@id='c_name']").click()
         self.driver.find_element_by_xpath("//*[@id='c_name']").send_keys("Abszint")
-        #Brutto beszerzesi egysegar megadasa
-        self.driver.find_element_by_xpath("//*[@id='c_purchase_price']").send_keys("1000")
-        # ME
+        #ME
         self.driver.find_element_by_xpath("//*[@id='tabs-base']/div[1]/div/div/button").click()
         self.driver.find_element_by_xpath("//*[@id='tabs-base']/div[1]/div/div/div/ul/li[2]").click()
-        #Nyitomennyiseg megadasa
-        self.driver.find_element_by_xpath("//*[@id='c_open_qty']").send_keys("10")
-        # Raktar
+        #Raktar
         self.driver.find_element_by_xpath("//*[@id='tabs-base']/div[4]/div/button").click()
         self.driver.find_element_by_xpath("//*[@id='tabs-base']/div[4]/div/div/ul/li[2]/label").click()
-        # Mentes
+        #Mentes
         self.driver.find_element_by_xpath("//*[@id='save']").click()
-        # visszaváltunk a böngészőre az iframe-ről
+        #visszaváltunk a böngészőre az iframe-ről
+        self.driver.switch_to.default_content()
+        self.assertTrue(self.driver.find_element_by_xpath("//td[contains(., 'Abszint')]").is_displayed())
+
+        #szerkesztés
+        self.driver.find_element_by_xpath("//td[contains(., 'Abszint')]//following::a").click()
+        self.driver.find_element_by_class_name("edit").click()
+        self.driver.switch_to.frame(self.driver.find_element_by_tag_name("iframe"))
+        self.driver.find_element_by_xpath("//*[@id='c_purchase_price']").send_keys('1010')
+        self.driver.find_element_by_xpath("//*[@id='save']").click()
         self.driver.switch_to.default_content()
         self.driver.refresh()
-        # számított mezők értékeinek ellenőrzése
         sleep(2)
-        mennyiseg = self.driver.find_element_by_xpath("//*[@id='components']/tbody//tr[1]/td[3]").text
-        self.assertEqual("10.00",mennyiseg)
-        nettoar = self.driver.find_element_by_xpath("//*[@id='components']/tbody//tr[1]/td[5]").text
-        self.assertEqual(nettoar,"787.40")
-        nettoertek=self.driver.find_element_by_xpath("//*[@id='components']/tbody//tr[1]/td[7]").text
-        self.assertEqual(nettoertek,"7 874.02")
-        #raktarak ellenörés
-
-        self.driver.find_element_by_xpath("//td[contains(., 'Abszint')]//following::a").click()
-        self.driver.find_element_by_class_name("storages").click()
-        self.driver.switch_to.frame(self.driver.find_element_by_tag_name("iframe"))
-        raktar = self.driver.find_element_by_xpath("/html/body/div[2]/table/tbody/tr[2]/td[2]").text
-        self.assertEqual(raktar,"Pult")
-        brutBesz = self.driver.find_element_by_xpath("/html/body/div[2]/table/tbody/tr[2]/td[3]").text
-        self.assertEqual(brutBesz,"1000")
-        menny =self.driver.find_element_by_xpath("/html/body/div[2]/table/tbody/tr[2]/td[4]").text
-        self.assertEqual(menny,"10")
-        raktErtek = self.driver.find_element_by_xpath("//html/body/div[2]/table/tbody/tr[2]/td[5]").text
-        self.assertEqual(raktErtek,"10000")
-        self.driver.find_element_by_tag_name("body").send_keys(Keys.ESCAPE)
-        self.driver.switch_to.default_content()
-        self.driver.find_element_by_xpath("/html/body/section/div/a[3]/span").click()
+        uj = self.driver.find_element_by_xpath("//*[@id='components']/tbody//tr[1]/td[6]").text
+        self.assertEqual('1 010.00', uj)
 
         #törlés
-        sleep(2)
         self.driver.find_element_by_xpath("//td[contains(., 'Abszint')]//following::a").click()
         self.driver.find_element_by_class_name("del").click()
 
         self.driver.find_element_by_xpath("//button[contains(.,'Igen')]").click()
-        self.driver.implicitly_wait(2)
-
-        self.driver.refresh()
-
-        with self.assertRaises(NoSuchElementException):
-            self.driver.find_element_by_xpath("//td[contains(., 'Abszint')]")
-
 
         self.driver.close()
